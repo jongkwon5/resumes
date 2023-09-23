9851949c77d98dd2ecd985708ccdb960213ec246
Parkjongkwon <jongkwong@naver.com>
2023-09-23 12:55:31 +0900

    12:55

--- component/footer/index.tsx
+++ component/footer/index.tsx
@@ -1,7 +1,6 @@
 import { Col, Row } from 'reactstrap';
 import { PropsWithChildren } from 'react';
 import { EmptyRowCol, HrefTargetBlank } from '../common';
-
 import { IFooter } from './IFooter';
 import { Style } from '../common/Style';
 
@@ -13,26 +12,15 @@ function Component({ payload }: PropsWithChildren<{ payload: IFooter.Payload }>)
   return (
     <Row>
       <Col style={Style.footerCover}>
-        <div style={Style.footer} className="text-center mt-2">
+        <div style={Style.footer} className="text-center mt-4">
           <EmptyRowCol>
-            <small>
-              v.{`${payload.version} / `}
+            <>
+              {payload.github}
+              박종권{' / '}
               {/* Github 주소는 origin repository 의 주소를 넣는다. */}
-              <HrefTargetBlank url="https://github.com/uyu423/resume-nextjs" text="Github" />
-              {' / '}
-              <HrefTargetBlank url="https://getbootstrap.com" text="Bootstrap" /> v
-              {payload.bootstrapVersion}
-            </small>
+              <HrefTargetBlank url="https://github.com/jongkwon5/rto" text="Resume" />
+            </>
           </EmptyRowCol>
-          <EmptyRowCol>
-            <small>
-              <HrefTargetBlank url="https://nextjs.org/" text="Next.js" /> v{payload.nextVersion}
-              {' / '}
-              <HrefTargetBlank url="https://reactjs.org/" text="React.js" /> v{payload.reactVersion}
-              {' / '}
-            </small>
-          </EmptyRowCol>
-          <br />
         </div>
       </Col>
     </Row>
